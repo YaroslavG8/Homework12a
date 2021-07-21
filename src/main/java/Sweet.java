c0e1da2366324e96aa6eeba4918982cfd00224da
Yaroslav <setevoeo@gmail.com>
2021-07-21 20:22:29 +0300

    First commit1

---
+++ src/main/java/Sweet.java
@@ -0,0 +1,18 @@
+public class Sweet {
+    int weight;
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public Sweet() {
+        this.weight = weight;
+    }
+
+    public void weight(int i) {
+    }
+}
